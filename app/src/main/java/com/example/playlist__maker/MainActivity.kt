902a38edabba95acc8cba1f7f6330d107f237b00
vixisss <shkanovaviktoria@mail.ru>
2024-08-30 17:01:14 +0400

    Домашнее задание

--- app/src/main/java/com/example/playlist__maker/MainActivity.kt
+++ app/src/main/java/com/example/playlist__maker/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.playlist__maker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.playlist__maker.R.id.media_btn
+import com.example.playlist__maker.R.id.search_btn
+import com.example.playlist__maker.R.id.settings_btn
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonSearch = findViewById<Button>(search_btn)
+        val buttonMedia = findViewById<Button>(media_btn)
+        val buttonSettings = findViewById<Button>(settings_btn)
+
+        buttonSearch.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
         }
+
+        buttonMedia.setOnClickListener {
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+        buttonSettings.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+
     }
 }
