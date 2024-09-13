fed20ec9aeb645936f5daba5761cda85852a60e4
vixisss <shkanovaviktoria@mail.ru>
2024-09-13 15:59:28 +0400

    Спринт №9

--- app/src/main/java/com/example/playlist__maker/SearchActivity.kt
+++ app/src/main/java/com/example/playlist__maker/SearchActivity.kt
@@ -1,11 +1,81 @@
 package com.example.playlist__maker
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
+
 class SearchActivity : AppCompatActivity() {
+    private var saveText: String = FIRST_STRING
+
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+        const val FIRST_STRING = ""
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, saveText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        saveText = savedInstanceState.getString(SEARCH_TEXT, FIRST_STRING)
+    }
+
+
+    @SuppressLint("MissingInflatedId", "ClickableViewAccessibility")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        //"Назад"
+        val rollbackBtn = findViewById<Button>(R.id.rollback_search)
+        rollbackBtn.setOnClickListener {
+            finish()
+        }
+
+        //"Поиск"
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val searchEditText = findViewById<EditText>(R.id.editText_Search)
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        }
+        searchEditText.addTextChangedListener(simpleTextWatcher)
+
+        //очистка содержимого editText
+        clearButton.setOnTouchListener { v, event ->
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    searchEditText.text.clear()
+                    hideKeyboard()
+                }
+            }
+            v?.onTouchEvent(event) ?: true
+        }
     }
+
+
+    private fun hideKeyboard() {
+        val searchEditText = findViewById<EditText>(R.id.editText_Search)
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+    }
+
 }
--- app/src/main/java/com/example/playlist__maker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlist__maker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.playlist__maker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +9,49 @@ import com.example.playlist__maker.R.id.rollback
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        //"назад"
         val rollbackBtn = findViewById<Button>(rollback)
 
         rollbackBtn.setOnClickListener {
             finish()
         }
+
+        // "Поделиться приложением"
+        val shareButton = findViewById<Button>(R.id.btn_shareApp)
+        shareButton.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.settingsShare_Text))
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        //"Написать в поддержку"
+        val supportHelpButton = findViewById<Button>(R.id.btn_supportHelp)
+        supportHelpButton.setOnClickListener{
+            val message = getString(R.string.supportHelpBodyLetter_text)
+            val theme = getString(R.string.supportHelpTheme_text)
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.myEmail)))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, theme)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+        //"Пользовательское соглашение"
+        val userAgreeButton = findViewById<Button>(R.id.btn_userAgree)
+        userAgreeButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(getString(R.string.offerYandex))
+            }
+            startActivity(intent)
+        }
+
     }
 }
