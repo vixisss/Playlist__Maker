c92eff0e20ed6b4121e7f74e7f6a03c05a7fafe8
vixisss <shkanovaviktoria@mail.ru>
2024-10-15 19:54:59 +0400

    Спринт 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,11 @@ android {
 }
 
 dependencies {
-    implementation("com.github.bumptech.glide:glide:4.14.2")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+    implementation(libs.glide)
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -47,4 +50,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(kotlin("script-runtime"))
 }
